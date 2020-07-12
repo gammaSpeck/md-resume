21e9d0f6de640011ba129562f5d6ac8846736a37
Madhu KM <madhukm3141@gmail.com>
2020-07-12 21:44:28 +0530

    Updates

---
+++ precache-manifest.bc2ab5db7d40bbfcf40d8581fb3e4d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c606ce1d7fffefeeea2d9ac1f665612a",
+    "url": "/md-resume/index.html"
+  },
+  {
+    "revision": "5ca664442ec782a537ba",
+    "url": "/md-resume/static/css/main.f6df5039.chunk.css"
+  },
+  {
+    "revision": "df5a4d5ad1fb58bd4f26",
+    "url": "/md-resume/static/js/2.7f1eaee8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/md-resume/static/js/2.7f1eaee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca664442ec782a537ba",
+    "url": "/md-resume/static/js/main.5c384051.chunk.js"
+  },
+  {
+    "revision": "2e3749f67546eb0a50e7",
+    "url": "/md-resume/static/js/runtime-main.2962cea9.js"
+  },
+  {
+    "revision": "bd328fd24be8c410ba90aa122578a675",
+    "url": "/md-resume/static/media/headshot.bd328fd2.png"
+  }
+]);
