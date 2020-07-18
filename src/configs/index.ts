e100168bc3981bda55ea076f11353497aad54944
Madhu KM <madhukm3141@gmail.com>
2020-07-18 21:07:05 +0530

    Removed comments from JSON

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,7 +2,7 @@ const config = () => {
   return {
     resumePublicURL:
       process.env.RESUME_PUBLIC_URL ||
-      'https://r1aw.githubusercontent.com/gammaSpeck/md-resume/master/public/resume.json',
+      'https://raw.githubusercontent.com/gammaSpeck/md-resume/master/public/resume.json',
     publicURL: process.env.PUBLIC_URL || 'http://localhost:3000'
   }
 }
